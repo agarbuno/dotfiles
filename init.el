839a657c468e9c9e3852855dc67d2e1db11e5e04
agarbuno <alfredo.garbuno@itam.mx>
2021-06-01 20:12:26 -0500

    agrega segundo tutorial

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
 
 ;; Change font size
 ;;(set-face-attribute 'default nil :font "Fira Code Retina" :height 280)
-
+;; Change default themes
 (load-theme 'wombat)
 
 ;; Make ESC quit prompts
@@ -37,8 +37,17 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(use-package command-log-mode)
+(column-number-mode)
+(global-display-line-numbers-mode t)
 
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+		shell-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(use-package command-log-mode)
 
 (use-package ivy
   :diminish
@@ -58,17 +67,44 @@
   :config
   (ivy-mode 1))
 
+(use-package ivy-rich
+  :after ivy
+  :init
+  (ivy-rich-mode 1))
+
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 15)))
 
-(column-number-mode)
-(global-display-line-numbers-mode t)
+;; Enables delimiters with colors in prog mode
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
 
-;; Disable line numbers for some modes
-(dolist (mode '(org-mode-hook
-                term-mode-hook
-		shell-mode-hook
-                eshell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil) ;; This cancels the ^ in the search
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+(use-package doom-themes
+   :init (load-theme 'doom-palenight t
