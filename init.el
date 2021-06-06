d13f104d42403b13a2549f4a4779680b1d403916
agarbuno <alfredo.garbuno@itam.mx>
2021-06-06 16:10:29 -0500

    agrega auto-update

--- init.el
+++ init.el
@@ -34,6 +34,15 @@
 (setq auto-save-file-name-transforms
       `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
 
+(use-package auto-package-update
+  :custom
+  (auto-package-update-interval 7)
+  (auto-package-update-prompt-before-update t)
+  (auto-package-update-hide-results t)
+  :config
+  (auto-package-update-maybe)
+  (auto-package-update-at-time "09:00"))
+
 (setq inhibit-startup-message t)
 
 (scroll-bar-mode -1)        ; Disable visible scrollbar
