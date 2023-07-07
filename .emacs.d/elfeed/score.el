de44bf4ab5e17a9158af1ae6a789f0e39f4c1678
agarbuno <alfredo.garbuno@itam.mx>
2023-07-07 10:43:00 -0600

    Updates scorefile

--- .emacs.d/elfeed/score.el
+++ .emacs.d/elfeed/score.el
@@ -5,13 +5,19 @@
  ("title-or-content"
   (:text "gaussian process" :title-value 20 :content-value 20 :type s)
   (:text "bayesian" :title-value 15 :content-value 15 :type s)
+  (:text "inverse problems" :title-value 10 :content-value 10 :type s)
   (:text "uncertainty quantification" :title-value 10 :content-value 10 :type s)
   (:text "uncertainty" :title-value 5 :content-value 5 :type s)
   (:text "variational inference" :title-value 10 :content-value 10 :type s)
   (:text "physics-informed" :title-value 10 :content-value 10 :type s)
   (:text "kalman" :title-value 5 :content-value 5 :type s)
   (:text "rkhs" :title-value 5 :content-value 5 :type s)
-  (:text "optimal transport" :title-value 5 :content-value 5 :type s))
+  (:text "optimal transport" :title-value 5 :content-value 5 :type s)
+  (:text "metropolis-hastings" :title-value 10 :content-value 10 :type s)
+  (:text "climate change" :title-value 10 :content-value 10 :type s)
+  (:text "climate" :title-value 10 :content-value 10 :type s)
+  (:text "data-driven" :title-value 10 :content-value 10 :type s)
+  (:text "data-based" :title-value 10 :content-value 10 :type s))
  ("tag"
   (:tags
    (t . stats)
@@ -26,7 +32,8 @@
    (t . siam)
    :value 4))
  ("authors"
-  (:text "stuart" :value 8 :type s))
+  (:text "stuart" :value 8 :type s)
+  (:text "schiechl" :value 8 :type s))
  ("feed")
  ("link")
  ("udf")
